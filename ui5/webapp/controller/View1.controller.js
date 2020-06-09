3f7bef73345017069c2516e8413837fd3fc0ada4
Aleksei Kosolapov <klealekle@gmail.com>
2020-06-09 11:40:55 +0000

    digest save to file comment

--- ui5/webapp/controller/View1.controller.js
+++ ui5/webapp/controller/View1.controller.js
@@ -1099,36 +1099,32 @@ sap.ui.define([
 						dataToSign = data;
 						console.log("digest: ", data);
 						
-						//savefile
-						alert("Сохраняем файл digest после подписи");
-                        var filename = "digest";
-						var type = xhr.getResponseHeader('Content-Type');
-						var blob = new Blob([data], { type: type });
+						// savefile
+						// alert("Сохраняем файл digest после подписи");
+    					// var filename = "digest";
+						// var blob = new Blob([data], { type: "text/plain" });
 
-						if (typeof window.navigator.msSaveBlob !== 'undefined') {
-							// IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
-							window.navigator.msSaveBlob(blob, filename);
-						} else {
-							var URL = window.URL || window.webkitURL;
-							var downloadUrl = URL.createObjectURL(blob);
+						// if (typeof window.navigator.msSaveBlob !== 'undefined') {
+						// 	window.navigator.msSaveBlob(blob, filename);
+						// } else {
+						// 	var URL = window.URL || window.webkitURL;
+						// 	var downloadUrl = URL.createObjectURL(blob);
 
-							if (filename) {
-								// use HTML5 a[download] attribute to specify filename
-								var a = document.createElement("a");
-								// safari doesn't support this yet
-								if (typeof a.download === 'undefined') {
-									window.location.href = downloadUrl;
-								} else {
-									a.href = downloadUrl;
-									a.download = filename;
-									document.body.appendChild(a);
-									a.click();
-								}
-							} else {
-								window.location.href = downloadUrl;
-							}
-							setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
-						}
+						// 	if (filename) {
+						// 		var a = document.createElement("a");
+						// 		if (typeof a.download === 'undefined') {
+						// 			window.location.href = downloadUrl;
+						// 		} else {
+						// 			a.href = downloadUrl;
+						// 			a.download = filename;
+						// 			document.body.appendChild(a);
+						// 			a.click();
+						// 		}
+						// 	} else {
+						// 		window.location.href = downloadUrl;
+						// 	}
+						// 	setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
+						// }
 
 						 ///////////////////
 						var thenable = that._SignCreate(Thumbprint, dataToSign);
